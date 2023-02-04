e8939040ac7133445b2501d450b2b8db6e1eb62f
Denzel Turambi <dturambi26@gmail.com>
2023-02-04 14:01:36 -0700

    Finalize meals.js

--- src/meal.js
+++ src/meal.js
@@ -19,31 +19,46 @@ function createMenuItem(itemName, itemPrice, itemType) {
   };
 
 // console.log(createMenuItem())
-// for loop n < #elements in an array 
-// if ingredientName === n element 
-  //dont do anything 
-// if ingredientName does not equal n element
-  // foodArray.push(ingredientName)
-function addIngredients(ingredientName, ingredients) {
-  var foodArray = ingredients;
-  // for (var i = 0; i < (foodArray.length -1); i++) {
-  //   if (ingredientName !== foodArray[i]) {
-      foodArray.push(ingredientName);
-  //   }
-  //  } 
-   return foodArray;  
-  };
-  
-  
 
+// function addIngredients(ingredientName, ingredients) {
+//   var foodArray = ingredients;
+//   // for (var i = 0; i < (foodArray.length -1); i++) {
+//   //   if (ingredientName !== foodArray[i]) {
+//       foodArray.push(ingredientName);
+//   //   }
+//   //  } 
+//    return foodArray;  
+//   };
+  
+function addIngredients(ingredients, ingredientList) {
+  if(ingredientList.includes(ingredients) === false) {
+      return ingredientList.push(ingredients);
+    } 
+};
+  
+function formatPrice(cost, fullcost) {
+  return fullcost = `$${cost}`
+};
 
+function decreasePrice(price, reducedPrice) {
+  return reducedPrice = price * .9
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
