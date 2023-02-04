fe6a083f9c2ccdab949596eadca13a2433aad29b
Denzel Turambi <dturambi26@gmail.com>
2023-02-04 12:11:13 -0700

    Add first half of tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
+// function nameMenuItem() {
+//   var food = "Pizza"
+//   return (`Delicious ${food}`);
+// };
+function nameMenuItem(foodName) {
+  var food = foodName;
+  return (`Delicious ${food}`);
+};
 
-}
+// nameMenuItem("French Toast");
+
+function createMenuItem(itemName, itemPrice, itemType) {
+  var menuItem = {
+    name: itemName,
+    price: itemPrice,
+    type: itemType
+    }
+    return menuItem;
+  };
+
+// console.log(createMenuItem())
+// for loop n < #elements in an array 
+// if ingredientName === n element 
+  //dont do anything 
+// if ingredientName does not equal n element
+  // foodArray.push(ingredientName)
+function addIngredients(ingredientName, ingredients) {
+  var foodArray = ingredients;
+  // for (var i = 0; i < (foodArray.length -1); i++) {
+  //   if (ingredientName !== foodArray[i]) {
+      foodArray.push(ingredientName);
+  //   }
+  //  } 
+   return foodArray;  
+  };
+  
+  
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
 }
-
 
