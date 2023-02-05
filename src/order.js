0f5c612ace4435dea23f5c73f586bee2dd45a69b
Denzel Turambi <dturambi26@gmail.com>
2023-02-05 13:10:40 -0700

    Add order and order-test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,41 @@
+function takeOrder(orderNum, deliveryOrders) {
+  if (deliveryOrders.length >= 3) {
+    return deliveryOrders;
+  }
+  deliveryOrders.push(orderNum);
+  return deliveryOrders;
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if(deliveryOrders[i].orderNumber === orderNumber) {
+      return deliveryOrders.splice(i, 1)
+    }
+  }
+}
+
+function listItems (item) {
+  var newString = ''
+  for (var i = 0; i < item.length; i++) {
+    newString += item[i].item
+    if (i !== item.length -1)
+      newString += `, `
+  }
+  return newString
+}
+
+function searchOrder(deliveryOrders, itemName) {
+for (var i = 0; i < deliveryOrders.length; i++) 
+  if (deliveryOrders[i].item === itemName) {
+    return true
+  }
+  return false
+}
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
